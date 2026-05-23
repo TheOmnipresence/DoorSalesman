6679037456f080a145b04d40d93e3552307e1a3e
TheOmnipresence <ItsTheOmnipresence@gmail.com>
2026-05-23 15:46:12 -0400

    clickable dialouge

---
+++ Scripts/clickable_dialogue.gd
@@ -0,0 +1,21 @@
+class_name ClickableDialogue extends NPC
+
+
+var mouse_hovering := false:
+	set(value):
+		mouse_hovering = value
+		if value:
+			$Polygon2D.color = Color(1,1,1,0.2)
+		else:
+			$Polygon2D.color = Color.TRANSPARENT
+
+
+func _ready() -> void:
+	$Polygon2D.polygon = $CollisionPolygon2D.polygon
+	mouse_entered.connect(func(): mouse_hovering = true)
+	mouse_exited.connect(func(): mouse_hovering = false)
+
+
+func _process(_delta: float) -> void:
+	if mouse_hovering and Input.is_action_just_pressed("mouse1") and not Globals.in_dialogue:
+		enter_dialouge()
