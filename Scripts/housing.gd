f3dc15323886d29d3db87823384fcf2e252a8016
TheOmnipresence <ItsTheOmnipresence@gmail.com>
2026-05-24 21:49:29 -0400

    housing stuff

--- Scripts/housing.gd
+++ Scripts/housing.gd
@@ -1,7 +1,15 @@
 extends Control
 
 
-var slide_index: int = 2
+@export var slide_index: int = 0:
+	set(value):
+		if value >= 0 and value < len($Slides.get_children()):
+			slide_index = value
+			
+			for i in $Slides.get_children():
+				i.hide()
+			
+			$Slides.get_child(value).show()
 
 func _ready() -> void:
 	get_node("Right").pressed.connect(func(): slide_index += 1)
